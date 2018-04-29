4a638d3dfbf8cfcfef5238e0434e1cad002201ca
Daniel Bravo <bravoappdev@gmail.com>
2018-04-29 10:59:55 -0700

    Add test code to connect to mysql

--- index.php
+++ index.php
@@ -1,8 +1,37 @@
 <!DOCTYPE html>
 <?php
+
+$port = "3306";
+$hostname = "localhost";
+
+$dbhost = $hostname.":".$port;
+$dbuser = "phpWebEngine";
+$dbpass = "withheld";
+$dbname = "YSoccerDB";
+
+$dbconn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if ($dbconn->connect_errno) {
+  printf("Connect failed: %s<br>", $dbconn->connect_error);
+  exit();
+} else {
+    echo "connected succesfully<br>";
+    $query = "
+  SELECT
+      *
+  FROM
+      Roles";
+}
+
+if ($result = $dbconn->query($query)) {
+  while ($row = $result->fetch_array(MYSQLI_NUM)) {
+      var_dump($row);
+      echo "<br>";
+  }
+}
+
 require 'header.php';
 
-echo "<h1>Main page</h1>";
+echo "<h1>Welcome Message</h1>";
 
 require 'footer.php';
 ?>
