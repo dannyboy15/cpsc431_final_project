c4778e58c107ce356e0b109cc776caf17cf3bb5a
Daniel Bravo <bravoappdev@gmail.com>
2018-04-29 18:56:21 -0700

    Add dummy data

---
+++ ysoccer_dml.sql
@@ -0,0 +1,67 @@
+INSERT INTO Adult VALUES
+(101, 'Jane', 'Smith', 'janesmith45@gmail','7145556262', '2569 Apple St.', 'Fullerton', 'CA', '92831'),
+(102, 'Jack', 'Jones', 'janesmith45@gmail','7145556262', '2569 Apple St.', 'Fullerton', 'CA', '92831'),
+(103, 'Edmund', 'Vega', 'janesmith45@gmail','7145556262', '2569 Apple St.', 'Fullerton', 'CA', '92831'),
+(104, 'Amber', 'Morris', 'janesmith45@gmail','7145556262', '2569 Apple St.', 'Fullerton', 'CA', '92831'),
+(105, 'Betty', 'Flores', 'janesmith45@gmail','7145556262', '2569 Apple St.', 'Fullerton', 'CA', '92831'),
+(106, 'Edna', 'Walsh', 'janesmith45@gmail','7145556262', '2569 Apple St.', 'Fullerton', 'CA', '92831'),
+(107, 'Brad', 'Mendoza', 'janesmith45@gmail','7145556262', '2569 Apple St.', 'Fullerton', 'CA', '92831'),
+(108, 'Delores', 'Clayton', 'janesmith45@gmail','7145556262', '2569 Apple St.', 'Fullerton', 'CA', '92831'),
+(109, 'Tonya', 'Moody', 'janesmith45@gmail','7145556262', '2569 Apple St.', 'Fullerton', 'CA', '92831'),
+(110, 'Kathy', 'Casey', 'janesmith45@gmail','7145556262', '2569 Apple St.', 'Fullerton', 'CA', '92831'),
+(111, 'Everett', 'Clark', 'janesmith45@gmail','7145556262', '2569 Apple St.', 'Fullerton', 'CA', '92831');
+
+
+INSERT INTO UserAcct VALUES
+(NULL, 'janesmith45@gmail.com', 'password123', 'observer','101');
+
+
+INSERT INTO Child VALUES
+(NULL, 101, 'Ivan', 'Nash', 'Goalie', 1),
+(NULL, 102, 'Megan', 'Higgins', 'Player', 1),
+(NULL, 103, 'Sam', 'George', 'Player', 1),
+(NULL, 104, 'Olive', 'Allison', 'Player', 1),
+(NULL, 105, 'Marianne', 'Byrd', 'Player', 1),
+(NULL, 106, 'Dwight', 'Greer', 'Player', 1),
+(NULL, 107, 'Regina', 'Edwards', 'Player', 1),
+(NULL, 108, 'Elbert', 'Bass', 'Player', 1),
+(NULL, 109, 'Carolyn', 'Mcbride', 'Player', 1),
+(NULL, 110, 'Jessie', 'Simpson', 'Player', 1),
+(NULL, 111, 'Meredith', 'Murphy', 'Player', 1);
+
+
+INSERT INTO Matches VALUES
+(NULL, 'The Pleasant Seastars', 'CSUF Soccer Field', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', 2, 3, NULL, NULL),
+(NULL, 'The Gigantic Camels', 'CSUF Soccer Field', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', 2, 3, NULL, NULL),
+(NULL, 'The Axiomatic Raccoons', 'CSUF Soccer Field', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', 2, 3, NULL, NULL),
+(NULL, 'The Exclusive Rhinoceroses', 'CSUF Soccer Field', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', 2, 3, NULL, NULL),
+(NULL, 'The Impossible Humans', 'CSUF Soccer Field', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', 2, 3, NULL, NULL),
+(NULL, 'The Tenuous Cobras', 'CSUF Soccer Field', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', 2, 3, NULL, NULL),
+(NULL, 'The Forgetful Eels', 'CSUF Soccer Field', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', 2, 3, NULL, NULL),
+(NULL, 'The Internal Lyrebirds', 'CSUF Soccer Field', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', 2, 3, NULL, NULL),
+(NULL, 'The Female Ravens', 'CSUF Soccer Field', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', 2, 3, NULL, NULL),
+(NULL, 'The Weak Jackals', 'CSUF Soccer Field', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', 2, 3, NULL, NULL);
+
+
+INSERT INTO Practice VALUES
+(NULL, 101, 'CSUF Practice Fields', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', '03:00:00', '2018-04-29'),
+(NULL, 101, 'CSUF Practice Fields', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', '03:00:00', '2018-04-30'),
+(NULL, 101, 'CSUF Practice Fields', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', '03:00:00', '2018-05-01'),
+(NULL, 101, 'CSUF Practice Fields', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', '03:00:00', '2018-05-02'),
+(NULL, 101, 'CSUF Practice Fields', '2569 Apple St.', 'Fullerton', 'CA', 'USA', '92831', '03:00:00', '2018-05-03');
+
+
+INSERT INTO SnackSignup VALUES
+(NULL, 102, 'Juice'),
+(NULL, 103, 'Water'),
+(NULL, 104, 'Oranges'),
+(NULL, 105, 'Apples');
+
+
+INSERT INTO Carpool VALUES
+(NULL, 106, 'Monday'),
+(NULL, 107, 'Tuesday'),
+(NULL, 108, 'Wednesday'),
+(NULL, 109, 'Thursday'),
+(NULL, 110, 'Friday'),
+(NULL, 111, 'Saturday');
