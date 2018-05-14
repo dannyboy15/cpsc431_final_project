1634916f36330ac09e49e2a42b06abc6e99ae86a
Daniel Bravo <bravoappdev@gmail.com>
2018-05-14 10:34:03 -0700

    Add functionality to return a connection based on role type

--- mysql_conn.php
+++ mysql_conn.php
@@ -1,13 +1,27 @@
 <?php
 
 // create db connection
-// $dbhost = "localhost";
 $port = "3306";
 $hostname = "localhost";
 
 $dbhost = $hostname.":".$port;
-$dbuser = "phpWebEngine";
-$dbpass = "withheld";
+// $dbuser = "phpWebEngine";
+// $dbpass = "withheld";
 $dbname = "YSoccerDB";
+
+$users = array('observer', 'user', 'manager', 'admin', 'phpWebEngine');
+$pwds = array('observer' => 'withheld', 'user' => 'withheld', 'manager' => 'withheld', 'admin' => 'withheld', 'phpWebEngine' => 'withheld');
+
+// var_dump($users);
+function new_connection($role)
+{
+  if(!in_array($role, $GLOBALS['users'])) {
+    return false;
+  }
+
+  $dbuser = $role;
+  $dbpass = $GLOBALS['pwds'][$role];
+  return new mysqli($GLOBALS['dbhost'], $dbuser, $dbpass, $GLOBALS['dbname']);
+}
 
  ?>
