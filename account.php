47e6a3550b6d65ac6b38d5edf2fa3740a185b0f0
Daniel Bravo <bravoappdev@gmail.com>
2018-05-14 18:51:45 -0700

    Implement edit account

--- account.php
+++ account.php
@@ -9,57 +9,63 @@ if(!isset($_SESSION['id'])) {
 $title = 'Youth Soccer - Account';
 $active = "Account";
 require_once('header.php');
+require_once('mysql_conn.php');
+require_once('./sanitize.php');
 
+$dbconn = new_connection($_SESSION['role']);
 
-// require_once('mysql_conn.php');
-// $dbconn = new_connection('phpWebEngine');
-//
-// if (mysqli_connect_errno()) {
-//     printf("Connect failed: %s\n", mysqli_connect_error());
-//     exit();
-// }
-//
-// $query = "SELECT Opponent, Location, Score, TeamRank FROM Matches";
-// $result = $dbconn->query($query);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$query = "SELECT Name_First, Name_Last, Email Phone, Street, City, State, ZipCode FROM Adult  WHERE ID = (SELECT AdultID FROM UserAcct WHERE ID = ?)";
+$stmt = $dbconn->prepare($query);
+
+if ($stmt) {
+    $stmt->bind_param('s', $_SESSION['id']);
+    if (!$stmt->execute()) {
+        echo "SQL execution failed";
+    } else {
+      // echo "It worked!!!!!";
+    }
+} else {
+  echo "statement failed";
+}
+
+if(($result = $stmt->get_result())->num_rows > 0) {
+  $row = $result->fetch_array(MYSQLI_ASSOC);
+
+  $firstName = $row['Name_First'];
 
 ?>
 <div class="container">
   <h1>Account</h1>
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Opponent</th>
-        <th scope="col">Location</th>
-        <th scope="col">Score</th>
-        <th scope="col">Team Rank</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      // if ($result) {
-      //     $row_num = 1;
-      //     while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-      //       printf ("<tr>
-      //                 <th scope=\"row\">%d</th>
-      //                 <td>%s</td>
-      //                 <td>%s</td>
-      //                 <td>%s</td>
-      //                 <td>%s</td>
-      //               </tr>",
-      //       $row_num++, $row['Opponent'], $row['Location'], $row['Score'], $row['TeamRank']);
-      //   }
-      // } else {
-      //   echo "query failed";
-      // }
-      //
-      // $dbconn->close();
-
-
-       ?>
-
-
-    </tbody>
-  </table>
+  <form action="account.php" method="POST">
+    <div class="form-group">
+      <label for="firstName">First Name</label>
+      <input type="firstName" class="form-control" id="firstName" name='firstName' value=<?= $firstName ?>>
+    </div>
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input type="password" class="form-control" id="password" name='password' placeholder="********">
+    </div>
+    <div>
+      <a href="display_forgot.php">Forgot Password</a> | <a href="display_registration.php">Register</a>
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 </div>
-<?php require_once('footer.php'); ?>
+<?php
+} else {
+  ?>
+  <div class="container">
+    <h1>Account</h1>
+    <p>Your account is not connected to and Adult profile. Ask the admin to connect your account.
+  </div>
+<?php
+}
+$dbconn->close();
+require_once('footer.php');
+?>
